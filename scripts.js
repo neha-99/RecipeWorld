7561365103bd6b77de1d541f5960f503a458ed3a
Neha Chugwani <nehachugwani9@gmail.com>
2020-07-18 19:47:02 +0530

    final

---
+++ scripts.js
@@ -0,0 +1,50 @@
+ $("document").ready(function(){
+            $("#reserve-modal").click(function()
+            {
+                $('#reserveModal').modal('show')
+            });
+
+            $("#modal-login").click(function()
+            {
+                $('#loginModal').modal('show')
+            });
+
+
+        });
+
+ $("#carouselButton").click(function(){
+                if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                    $("#mycarousel").carousel('pause');
+                    $("#carouselButton").children("span").removeClass('fa-pause');
+                    $("#carouselButton").children("span").addClass('fa-play');
+                }
+                else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                    $("#mycarousel").carousel('cycle');
+                    $("#carouselButton").children("span").removeClass('fa-play');
+                    $("#carouselButton").children("span").addClass('fa-pause');                    
+                }
+            });
+
+ $(document).ready(function() {
+    // jQuery code
+
+    //////////////////////// Prevent closing from click inside dropdown
+    $(document).on('click', '.dropdown-menu', function (e) {
+      e.stopPropagation();
+    });
+
+    // make it as accordion for smaller screens
+    if ($(window).width() < 992) {
+        $('.dropdown-menu a').click(function(e){
+            e.preventDefault();
+            if($(this).next('.submenu').length){
+                $(this).next('.submenu').toggle();
+            }
+            $('.dropdown').on('hide.bs.dropdown', function () {
+               $(this).find('.submenu').hide();
+            })
+        });
+    }
+    
+});
+                
